87ac0e348882dc544c09258bf26adf677b72aeec
charleswang234 <wang.charles234@gmail.com>
2018-06-19 21:08:16 +0000

    changed colour to red if over character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,13 @@
 $(document).ready(function() {
   $('.new-tweet textarea').keyup(function() {
     var $textarea = $(this);
-    var charactersLeft = 140 - $textarea.val().length
-    console.log(charactersLeft);
-
-    // console.log($textarea.parent());
-    console.log($textarea.next().children('.counter').html(charactersLeft));
+    var charactersLeft = 140 - $textarea.val().length;
+    $textarea.siblings(".counter").text(charactersLeft);
+    if (charactersLeft < 0) {
+      $textarea.siblings(".counter").css("color", "red");
+    } else {
+       $textarea.siblings(".counter").css("color", "black");
+    }
   });
 });
 
